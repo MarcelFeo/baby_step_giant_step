dade4052065c77d3f0fccef4a252c9e02a1e9e04
MarcelFeo <marcelfeo29@gmail.com>
2023-07-04 19:16:31 -0300

    feat: add user input

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
-print("Ok.")
+from math import sqrt, ceil
+
+p = input("Digite um número primo: ")
+a = input("Digite um gerador do grupo multiplicativo módulo p: ")
+b = input("Digite o valor para encontrar o expoente 'x': ")
