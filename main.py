11167ca5ff98f539e31fb0212f5f2be1ce670610
MarcelFeo <marcelfeo29@gmail.com>
2023-07-04 19:23:25 -0300

    feat: add function check prime

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 from math import sqrt, ceil
 
-p = input("Digite um número primo: ")
-a = input("Digite um gerador do grupo multiplicativo módulo p: ")
-b = input("Digite o valor para encontrar o expoente 'x': ")
+def eh_primo(n):
+    for count in range(2,n):
+        if(n % count == 0):
+            return False
+
+    return True
+
+
+p = int(input("Digite um número primo: "))
+a = int(input("Digite um gerador do grupo multiplicativo módulo p: "))
+b = int(input("Digite o valor para encontrar o expoente 'x': "))
+
+if(eh_primo(p)):
+    print("é primo!")
+else:
+    print("não é primo!")
