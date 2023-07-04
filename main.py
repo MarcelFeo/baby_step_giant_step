1ffe8ea5b2edc38249b167f42a136220d2ac3371
MarcelFeo <marcelfeo29@gmail.com>
2023-07-04 19:26:13 -0300

    feat: add baby step

--- main.py
+++ main.py
@@ -7,12 +7,32 @@ def eh_primo(n):
 
     return True
 
+def baby_step_giant_step(a, b, p):
+    # Verifica se 'a' é um gerador do grupo multiplicativo módulo 'p'
+    if pow(a, (p - 1) // 2, p) != 1:
+        raise ValueError("'a' não é um gerador do grupo multiplicativo módulo 'p'")
+
+    # Calcula o tamanho aproximado da tabela
+    m = ceil(sqrt(p - 1))
+
+    # Baby step: cria uma tabela de baby steps
+    baby_steps = {}
+    val = 1
+    for i in range(m):
+        if val not in baby_steps:
+            baby_steps[val] = i
+        val = (val * a) % p
+
+    # Calcula o inverso multiplicativo de a^(m)
+    inv_a_m = pow(pow(a, m, p), p - 2, p)
+
 
 p = int(input("Digite um número primo: "))
 a = int(input("Digite um gerador do grupo multiplicativo módulo p: "))
 b = int(input("Digite o valor para encontrar o expoente 'x': "))
 
 if(eh_primo(p)):
-    print("é primo!")
+    x = baby_step_giant_step(a, b, p)
+    print(f"O valor de 'x' é: {x}")
 else:
     print("não é primo!")
